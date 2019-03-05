19cb58f2a42f71fa482de23ee108707e462075a1
chinoad <46451027+chinoad@users.noreply.github.com>
2019-03-05 12:26:39 -0500

    Update WhiteBox2.java
    Found more efficient way to do this.

--- src/WhiteBox2.java
+++ src/WhiteBox2.java
@@ -4,44 +4,27 @@ public class WhiteBox2 {
 
     public static void main(String[] args) {
 
-
-        int count = 0;
+        int count = 1;
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter your PIN");
         int pin = scanner.nextInt();
+        do {
 
-        for (int i = 1; i <= 3; i++) {
+            if(pin==1234){
+                System.out.println("Access granted");
+                System.exit(0);
 
-            if (pin != 1234) {
+            }else{
+                System.out.println("Wrong PIN. Please re-enter your pin");
+                pin = scanner.nextInt();
                 count++;
-                if (count < 3) {
-                    incorrectPin();
-                    scanner.nextInt();
-                }
-                if (count == 3) {
-                    strike();
-                    break;
-                }
-            } else if(pin == 1234) {
-                correctPin();
-                break;
             }
-        }
-    }
 
-    public static void correctPin() {
-        System.out.println("Access granted");
-    }
 
-    public static void incorrectPin() {
-        System.out.println("Wrong PIN. Please re-enter");
-
-    }
-
-    public static void strike() {
+        }while(count<3);
         System.out.println("Access denied");
-
     }
-
 }
+
+
